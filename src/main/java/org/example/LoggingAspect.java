7c3deb9b2c8829ed3797c4685927a914b5374350
aymenmoubtassimelidrissi <aelidrissimoubtassim@sqli.com>
2024-02-21 11:07:21 +0100

    initial commit

---
+++ src/main/java/org/example/LoggingAspect.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class LoggingAspect {
+    public void beforeAdvice() {
+        System.out.println("Before advice - Logging...");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MyService myService = (MyService) context.getBean("myService");
+        myService.doSomething();
+    }
+}
